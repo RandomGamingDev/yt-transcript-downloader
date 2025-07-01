48677f6d7ac34717d0b51095996e0f944fe488b5
RandomGamingDev <randomgamingdev@gmail.com>
2025-07-01 13:42:52 -0400

    fixed issue with button appearing greyed out on light mode

--- main.js
+++ main.js
@@ -52,7 +52,7 @@ new MutationObserver(function(mutationsList, observer) {
 		transcript_header.insertBefore(transcript_header_download_option, transcript_header_menu_option);
 		transcript_header_download_icon.insertAdjacentHTML("beforeend", `
 			<div style="width: 100%; height: 100%; display: block; fill: currentcolor;">
-				<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M480-320 280-520l56-58 104 104v-326h80v326l104-104 56 58-200 200ZM240-160q-33 0-56.5-23.5T160-240v-120h80v120h480v-120h80v120q0 33-23.5 56.5T720-160H240Z"/></svg>
+				<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"><path d="M480-320 280-520l56-58 104 104v-326h80v326l104-104 56 58-200 200ZM240-160q-33 0-56.5-23.5T160-240v-120h80v120h480v-120h80v120q0 33-23.5 56.5T720-160H240Z"/></svg>
 			</div>
 		`);
 		const transcript_header_download_button = transcript_header_download_option.querySelector("button#button");
